7673203616455e075b9616885f58d7b9e1c18170
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-02-20 19:58:37 -0600

    Missing number

--- Introductory-Problems/Missing-Number/a.cpp
+++ Introductory-Problems/Missing-Number/a.cpp
@@ -5,6 +5,21 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
+    
+    ll sum = 0;
+    ll total = 1;
+    int n;
+    
+    cin >> n;
+
+    ll x;
+    for(int i = 2; i<=n; i++){
+        cin >> x;
+        sum+=x;
+        total +=i;
+    }
+
+    cout << total - sum << endl;
+
     return 0;
 }
