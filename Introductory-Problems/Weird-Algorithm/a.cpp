8fe6d4f2f3fe263083660f6f071087f5bd58e960
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-02-20 19:50:37 -0600

    Weird Algorithm

--- Introductory-Problems/Weird-Algorithm/a.cpp
+++ Introductory-Problems/Weird-Algorithm/a.cpp
@@ -5,6 +5,18 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
+    ll n;
+    cin >> n;
+    
+    while(n != 1){
+        cout << n << " ";
+        if(n %2 == 0){
+            n /= 2;
+        }
+        else{
+            n = 3*n +1;
+        }
+    }
+    cout << n << "\n";     
     return 0;
 }
