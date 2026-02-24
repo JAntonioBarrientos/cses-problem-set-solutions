8e40cf0d93927b225a4d5e18fdd3228ce5316fa3
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-02-24 01:18:19 -0600

    nw

--- Introductory-Problems/Increasing-Array/a.cpp
+++ Introductory-Problems/Increasing-Array/a.cpp
@@ -5,6 +5,17 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
+    int n; 
+    cin >> n;
+    int prev;
+    cin >> prev;
+    ll ans = 0;
+    int curr;
+    for(int i = 1; i < n; i++){
+        cin >> curr;
+        prev = max(prev, curr);
+        ans += prev-curr;
+    }
+    cout << ans << endl;
     return 0;
 }
