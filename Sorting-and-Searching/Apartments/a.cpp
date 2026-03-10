cc32dd96a0fd321ed492d972bc723e53d9050eda
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-03-09 23:26:40 -0500

    Auto-sync: 2026-03-09 23:26:40

--- Sorting-and-Searching/Apartments/a.cpp
+++ Sorting-and-Searching/Apartments/a.cpp
@@ -5,10 +5,37 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin >> n;
-    
-    this was me in surface
-        and this from mac
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> aplicants(n);
+    vector<int> appartments(m);
+
+    for(int i =0; i < n; i++)
+        cin >> aplicants[i];
+    for(int i =0; i < m; i++)
+        cin >> appartments[i];
+
+    sort(aplicants.begin(), aplicants.end());
+    sort(appartments.begin(), appartments.end());
+
+    int idx_apli = 0;
+    int idx_app = 0;
+    int cnt= 0;
+    while(idx_apli < n and idx_app < m){
+        if(aplicants[idx_apli]-k<= appartments[idx_app] and appartments[idx_app] <= aplicants[idx_apli] + k){
+            cnt++;
+            idx_apli++;
+            idx_app++;
+        
+        }
+        else if(appartments[idx_app] < aplicants[idx_apli]-k){
+            idx_app++;
+        }
+        else{
+            idx_apli++;
+        }
+    }
+    cout << cnt << endl;
+
     return 0;
 }
--- Sorting-and-Searching/Ferris-Wheel/a.cpp
+++ Sorting-and-Searching/Ferris-Wheel/a.cpp
@@ -5,6 +5,28 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int n, x;
+    cin >> n>> x;
+    vector<int> children(n);
+    for(int i =0; i < n;i++)
+        cin >> children[i];
+
+    int ans = 0;
+    sort(children.begin(), children.end());
+
+    int p_end = n-1;
+    int p_beg = 0;
+
+    while(p_beg <= p_end){
+        if(children[p_end] + children[p_beg] <= x)
+            p_beg++;
+        p_end--;
+        ans++;
+    }
+
+
+    cout << ans << endl;
+
    
     return 0;
 }
