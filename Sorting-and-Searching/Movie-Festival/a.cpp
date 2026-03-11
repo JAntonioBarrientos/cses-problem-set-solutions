750f1ef894834320249c946cd0e030ba1bae5128
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-03-10 23:05:47 -0500

    Auto-sync: 2026-03-10 23:05:47

--- Sorting-and-Searching/Movie-Festival/a.cpp
+++ Sorting-and-Searching/Movie-Festival/a.cpp
@@ -5,6 +5,40 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
+    int n, a, b;
+    cin >> n;
+    vector<tuple<int, int, int>> events;
+    vector<pair<int, int>> movies;
+    for(int i = 0; i< n; i++){
+        cin >> a >> b;
+        events.emplace_back(a, 0, i);
+        events.emplace_back(b, 1, i);
+        movies.emplace_back(a, b);
+    }
+    sort(events.begin(), events.end());
+    int watched = 0;
+    int current = -1;
+    for(const auto&[time, type, id]: events){
+        if(id == current and type == 1){
+            current = -1;
+            watched++;
+            continue;
+        }
+        if(type == 0){
+            if(current == -1){
+                current = id;
+            }
+            if(movies[current].second == time){
+                current = id;
+                watched++;
+                continue;
+            }
+            else if(movies[current].second > movies[id].second){
+                current = id;
+            }
+        }
+
+    }
+    cout << watched << endl;
     return 0;
 }
--- Sorting-and-Searching/Sum-of-Two-Values/a.cpp
+++ Sorting-and-Searching/Sum-of-Two-Values/a.cpp
@@ -2,9 +2,48 @@
 using namespace std;
 using ll = long long;
 
+int search(int i, int j, int x, const vector<pair<int,int>> &nums){
+    if(i > j){
+        return -1;
+    }
+    int m = (i+j)/2;
+    if(nums[m].first == x){
+        return nums[m].second;
+    }
+    if(x < nums[m].first){
+       return search(i, m-1, x, nums);
+    }
+    else{
+       return search(m+1, j, x, nums);
+    }
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
+    int n, sum;
+    cin >> n >> sum;
+    vector<pair<int, int>> nums;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        nums.emplace_back(x, i);
+    }
+
+    sort(nums.begin(), nums.end());
+    int res;
+    int l;
+    for(l = 0; l < n-1; l++){
+        int x = sum - nums[l].first;
+        res = search(l+1, n-1, x, nums);
+        if(res != -1)
+            break;
+    }
+    if(res == -1){
+        cout << "IMPOSSIBLE";
+    }
+    else{
+        cout << nums[l].second+1 << " " << res + 1 << endl;
+    }
     return 0;
 }
