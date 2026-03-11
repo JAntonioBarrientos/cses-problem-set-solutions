eeda7a8835a06f87122c44f4cc3e909754ba44de
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-03-11 14:06:19 -0600

    Auto-save 2026-03-11 14:06

--- Mathematics/Creating-Strings-II/a.cpp
+++ Mathematics/Creating-Strings-II/a.cpp
@@ -1,17 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+const ll MOD = 1e9+7;
 
+ll binpow(ll a, ll b){
+    if(b == 0)
+        return 1;
+    if(b & 1)
+        return (a * binpow(a, b-1)) % MOD;
+    ll res = binpow(a, b/2);
+    return (res * res) % MOD;
+}
+
+ll mod_inv(ll a){
+    return binpow(a, MOD-2);
+}
+
+vector<ll> fact(1e6+1);
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     string s;
     cin >> s;
     vector<int> freq(26);
-    for(char c: string){
-        freq[c-'0']++;
+    int n = s.size();
+    for(char c: s){
+        freq[c-'a']++;
     }
-
-   
+    fact[0] = 1;
+    for(int i = 1; i <=n; i++)
+        fact[i] = (fact[i-1] * i) % MOD;
+    ll denominador = 1;
+    for(int i = 0; i < freq.size(); i++)
+        denominador = (denominador * fact[freq[i]] ) % MOD;
+    ll res = (fact[n] * mod_inv(denominador)) % MOD;
+    cout << res << endl;
     return 0;
 }
--- Sorting-and-Searching/Playlist/a.cpp
+++ Sorting-and-Searching/Playlist/a.cpp
@@ -5,6 +5,32 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> songs(n);
+    int x;
+    for(int i = 0; i<n; i++){
+        cin >> songs[i];
+    }
+    map<int, int> window;
+    int m_size = 0;
+    int l = 0;
+    for(int r = 0; r<n; r++){
+        if(window.count(songs[r]) == 0){
+            window[songs[r]] = r; 
+            m_size = max(m_size, int(window.size()));
+        }
+        else{
+            int id = window[songs[r]];
+            for(int i = l; i <= id; i++){
+                window.erase(songs[i]);
+            }
+            window[songs[r]] = r;
+            l = id+1;
+        }
+    }
+
+    cout << m_size << endl;
    
     return 0;
 }
