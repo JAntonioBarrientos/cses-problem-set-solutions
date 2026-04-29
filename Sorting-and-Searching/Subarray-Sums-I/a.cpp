dc0d5e66a0a23d31af1158aefde5df3ce7b1d86e
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2026-04-29 02:47:59 -0600

    o

--- Sorting-and-Searching/Subarray-Sums-I/a.cpp
+++ Sorting-and-Searching/Subarray-Sums-I/a.cpp
@@ -5,6 +5,21 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
-    return 0;
+    int n,x;
+    cin >> n >> x;
+    vector<ll> nums(n);
+    for(auto &y: nums)
+        cin >> y;
+    int l = 0; ll sum = 0; ll cnt = 0;
+    for(int r = 0; r < n; r++){
+        sum += nums[r];
+        while(sum > x and l<=r){
+            sum -= nums[l];
+            l++;
+        }
+        if(sum == x)
+            cnt++;
+    }
+    cout << cnt << endl;
+   return 0;
 }
--- Sorting-and-Searching/Subarray-Sums-II/a.cpp
+++ Sorting-and-Searching/Subarray-Sums-II/a.cpp
@@ -5,6 +5,28 @@ using ll = long long;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-   
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> nums(n);
+    for(int i= 0; i <n; i++){
+        cin >> nums[i];
+    }
+    vector<ll> prefix(n+1);
+    prefix[0] = 0;
+    for(int i = 1; i <= n; i++){
+        prefix[i] = nums[i-1]+ prefix[i-1];
+    }
+    multiset<ll> ms;
+    for(auto p: prefix){
+        ms.insert(p);
+    }
+    ll cnt = 0;
+    for(int i = 0; i <= n; i++){
+        if(ms.count(x+prefix[i]) > 0){
+            cnt += ms.count(x+prefix[i]);
+        }
+        ms.erase(prefix[i]);
+    }
+    cout << cnt << endl;
     return 0;
 }
